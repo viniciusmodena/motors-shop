06315097f6da005f34e81f858d0367a879725288
Helashow <lucas.bnlsp2013@gmail.com>
2022-11-01 21:40:44 -0300

    feat: profile page and basic router structure

--- src/components/AdvertiseList/index.tsx
+++ src/components/AdvertiseList/index.tsx
@@ -3,25 +3,33 @@ import { ProductAuctionCard, ProductCard } from "../Card";
 
 export const AdvertiseList = ({ products, type }: any) => {
   return type === "auction" ? (
-    <Box id="auction" margin={["1rem 0.75rem 1rem", "2rem 2rem 2rem", "5rem 3rem 8rem"]}>
+    <Box
+      id="auction"
+      margin={["1rem 0.75rem 1rem", "2rem 2rem 2rem", "5rem 3rem 8rem"]}
+    >
       <Heading variant="h5" margin={["2rem 0", "2rem 0", "3rem 0"]}>
         Leilão
       </Heading>
-      <Flex gridGap={["0.75rem", "0.75rem", "1.5rem"]} overflowX="auto">
-        {products?.map((product: any) => {
-          return <ProductAuctionCard key={product.id} product={product} />;
-        })}
+      <Flex gridGap={["0.75rem", "0.75rem", "1.5rem"]} overflow="auto" paddingBottom="1rem">
+        {products &&
+          products?.map((product: any) => {
+            return <ProductAuctionCard key={product.id} product={product} />;
+          })}
       </Flex>
     </Box>
   ) : (
-    <Box id={type} margin={["1rem 0.75rem 1rem", "2rem 2rem 2rem", "5rem 3rem 8rem"]}>
+    <Box
+      id={type}
+      margin={["1rem 0.75rem 1rem", "2rem 2rem 2rem", "5rem 3rem 8rem"]}
+    >
       <Heading variant="h5" margin={["2rem 0", "2rem 0", "3rem 0"]}>
         {type === "bikes" ? "Motos" : "Carros"}
       </Heading>
-      <Flex gridGap={["0.75rem", "0.75rem", "2rem"]} overflowX="auto">
-        {products?.map((product: any) => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
+      <Flex gridGap={["0.75rem", "0.75rem", "2rem"]} overflow="auto" paddingBottom="1rem" >
+        {products &&
+          products?.map((product: any) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
       </Flex>
     </Box>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,22 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./style/globalStyle";
 import { AdvertiseListProvider } from "./Providers/AdvertiseList.provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import { ProfilePage } from "./routes/Profile";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AdvertiseListProvider>
       <ChakraProvider theme={theme}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="/" element={<Home />} />
+              <Route path="profile/:user_id" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ChakraProvider>
     </AdvertiseListProvider>
   </React.StrictMode>
